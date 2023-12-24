1a8a0f7c91beb6d0e6e76f567330cd6cdc91dd4d
Aayushman Sharma <075bme004aayushman@gmail.com>
2023-12-24 12:21:12 +0545

    Completed and Incomplete buttons and funtionalies are added

--- src/TodoList.ts
+++ src/TodoList.ts
@@ -6,6 +6,12 @@ export interface ITodoList {
   toggleFavorite(id: number): void;
   removeItem(id: number): void;
   render(): void;
+  getFavoriteItems(): TodoItem[];
+  getCompletedItems(): TodoItem[];
+  getIncompleteItems(): TodoItem[];
+  renderFavorites(): void;
+  renderCompleted(): void;
+  renderIncomplete(): void;
   items: TodoItem[];
 }
 
@@ -18,7 +24,7 @@ export class TodoList implements ITodoList {
 
   addItem(text: string): void {
     const newItem = new TodoItem(this.items.length + 1, text, false);
-    this.items.push(newItem);
+    this.items.unshift(newItem);
     this.render();
   }
 
@@ -43,52 +49,106 @@ export class TodoList implements ITodoList {
     this.render();
   }
 
+  getFavoriteItems(): TodoItem[] {
+    return this.items.filter((item) => item.favorite);
+  }
+
+  getCompletedItems(): TodoItem[] {
+    return this.items.filter((item) => item.completed);
+  }
+
+  getIncompleteItems(): TodoItem[] {
+    return this.items.filter((item) => !item.completed);
+  }
+
   render(): void {
-    const todoList = document.getElementById("todo-list");
-    if (todoList) {
-      todoList.innerHTML = "";
+    const todoContainer = document.getElementById("todoList");
+    if (todoContainer) {
+      todoContainer.innerHTML = "";
 
       this.items.forEach((item) => {
-        const listItem = document.createElement("li");
-
-        const toggleButton = document.createElement("button");
-        const iconElementComplete = document.createElement("i");
-        iconElementComplete.classList.add(
-          "bi",
-          item.completed ? "bi-check-circle-fill" : "bi-circle"
-        );
-        toggleButton.appendChild(iconElementComplete);
-        toggleButton.addEventListener("click", () => this.toggleItem(item.id));
-
-        const textElement = document.createElement("span");
-        textElement.textContent = item.text;
-        listItem.className = item.completed ? "completed" : "";
-        listItem.appendChild(textElement);
-
-        const favoriteButton = document.createElement("button");
-        const iconElementFavorite = document.createElement("i");
-        iconElementFavorite.classList.add(
-          "bi",
-          item.favorite ? "bi-star-fill" : "bi-star"
-        );
-        favoriteButton.appendChild(iconElementFavorite);
-        favoriteButton.addEventListener("click", () =>
-          this.toggleFavorite(item.id)
-        );
-
-        const removeButton = document.createElement("button");
-        const iconElementDelete = document.createElement("i");
-        iconElementDelete.classList.add("bi", "bi-trash");
-        removeButton.appendChild(iconElementDelete);
-        removeButton.addEventListener("click", () => this.removeItem(item.id));
-
-        listItem.appendChild(toggleButton);
-        listItem.appendChild(textElement);
-        listItem.appendChild(favoriteButton);
-        listItem.appendChild(removeButton);
-
-        todoList.appendChild(listItem);
+        const listItem = this.createListItem(item);
+        todoContainer.appendChild(listItem);
       });
     }
+  }
+
+  renderFavorites(): void {
+    const favoriteContainer = document.getElementById("favoriteList");
+    if (favoriteContainer) {
+      favoriteContainer.innerHTML = "";
+
+      const favoriteItems = this.getFavoriteItems();
+      favoriteItems.forEach((item) => {
+        const listItem = this.createListItem(item);
+        favoriteContainer.appendChild(listItem);
+      });
+    }
+  }
+
+  renderCompleted(): void {
+    const completedContainer = document.getElementById("completedList");
+    if (completedContainer) {
+      completedContainer.innerHTML = "";
+
+      const completedItems = this.getCompletedItems();
+      completedItems.forEach((item) => {
+        const listItem = this.createListItem(item);
+        completedContainer.appendChild(listItem);
+      });
+    }
+  }
+
+  renderIncomplete(): void {
+    const incompleteContainer = document.getElementById("incompleteList");
+    if (incompleteContainer) {
+      incompleteContainer.innerHTML = "";
+
+      const incompleteItems = this.getIncompleteItems();
+      incompleteItems.forEach((item) => {
+        const listItem = this.createListItem(item);
+        incompleteContainer.appendChild(listItem);
+      });
+    }
+  }
+
+  private createListItem(item: TodoItem): HTMLLIElement {
+    const listItem = document.createElement("li");
+
+    const toggleButton = document.createElement("button");
+    const iconElementComplete = document.createElement("i");
+    iconElementComplete.classList.add(
+      "bi",
+      item.completed ? "bi-check-circle-fill" : "bi-circle"
+    );
+    toggleButton.appendChild(iconElementComplete);
+    toggleButton.addEventListener("click", () => this.toggleItem(item.id));
+    listItem.appendChild(toggleButton);
+
+    const textElement = document.createElement("span");
+    textElement.textContent = item.text;
+    listItem.className = item.completed ? "completed" : "";
+    listItem.appendChild(textElement);
+
+    const favoriteButton = document.createElement("button");
+    const iconElementFavorite = document.createElement("i");
+    iconElementFavorite.classList.add(
+      "bi",
+      item.favorite ? "bi-star-fill" : "bi-star"
+    );
+    favoriteButton.appendChild(iconElementFavorite);
+    favoriteButton.addEventListener("click", () =>
+      this.toggleFavorite(item.id)
+    );
+    listItem.appendChild(favoriteButton);
+
+    const removeButton = document.createElement("button");
+    const iconElementDelete = document.createElement("i");
+    iconElementDelete.classList.add("bi", "bi-trash");
+    removeButton.appendChild(iconElementDelete);
+    removeButton.addEventListener("click", () => this.removeItem(item.id));
+    listItem.appendChild(removeButton);
+
+    return listItem;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,81 @@ import { TodoList } from "./TodoList";
 
 const todoList = new TodoList();
 
+function showAll() {
+  todoList.render();
+  toggleVisibility("todoList", true);
+  toggleVisibility("favoriteList", false);
+  toggleVisibility("completedList", false);
+  toggleVisibility("incompleteList", false);
+  console.log("all");
+}
+
+function showFavorites() {
+  console.log("favourites");
+  todoList.renderFavorites();
+  toggleVisibility("todoList", false);
+  toggleVisibility("favoriteList", true);
+  toggleVisibility("completedList", false);
+  toggleVisibility("incompleteList", false);
+}
+
+function showCompleted() {
+  todoList.renderCompleted();
+  toggleVisibility("todoList", false);
+  toggleVisibility("favoriteList", false);
+  toggleVisibility("completedList", true);
+  toggleVisibility("incompleteList", false);
+  console.log("completed");
+}
+
+function showIncomplete() {
+  todoList.renderIncomplete();
+  toggleVisibility("todoList", false);
+  toggleVisibility("favoriteList", false);
+  toggleVisibility("completedList", false);
+  toggleVisibility("incompleteList", true);
+  console.log("Incomplete");
+}
+
+function toggleVisibility(elementId: string, isVisible: boolean) {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.style.display = isVisible ? "block" : "none";
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const addTodoInput = document.getElementById(
     "addTodoInput"
   ) as HTMLInputElement;
   const addTodoButton = document.getElementById("addTodoButton");
+  const showAllButton = document.getElementById("showAllButton");
+  const showFavoritesButton = document.getElementById("showFavoritesButton");
+  const showCompletedButton = document.getElementById("showCompletedButton");
+  const showIncompleteButton = document.getElementById("showIncompleteButton");
 
   addTodoButton?.addEventListener("click", () => {
     if (addTodoInput.value.trim() !== "") {
-      console.log("Hellow world");
       todoList.addItem(addTodoInput.value);
       addTodoInput.value = "";
     }
   });
+
+  if (showAllButton) {
+    showAllButton.addEventListener("click", showAll);
+  }
+
+  if (showFavoritesButton) {
+    showFavoritesButton.addEventListener("click", showFavorites);
+  }
+
+  if (showCompletedButton) {
+    showCompletedButton.addEventListener("click", showCompleted);
+  }
+
+  if (showIncompleteButton) {
+    showIncompleteButton.addEventListener("click", showIncomplete);
+  }
+
+  showAll();
 });
