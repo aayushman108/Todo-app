14252d6caf898b102dd09b60fcc00963f6c37e7b
Aayushman Sharma <075bme004aayushman@gmail.com>
2023-12-25 09:54:12 +0545

    Working with styling of the app

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const showCompletedButton = document.getElementById("showCompletedButton");
   const showIncompleteButton = document.getElementById("showIncompleteButton");
 
+  const navButton = [
+    showAllButton,
+    showFavoritesButton,
+    showCompletedButton,
+    showIncompleteButton,
+  ];
+
+  const makeButtonStyleInactive = () => {
+    navButton.forEach((button) => {
+      if (button) {
+        button.style.border = "none";
+      }
+    });
+  };
+
   const searchInput = document.getElementById(
     "searchInput"
   ) as HTMLInputElement;
@@ -40,25 +55,36 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   if (showAllButton) {
-    showAllButton.addEventListener("click", () => todoList.render("todo"));
+    showAllButton.style.borderBottom = "5px solid blue";
+    showAllButton.addEventListener("click", () => {
+      todoList.render("todo");
+      makeButtonStyleInactive();
+      showAllButton.style.borderBottom = "5px solid blue";
+    });
   }
 
   if (showFavoritesButton) {
-    showFavoritesButton.addEventListener("click", () =>
-      todoList.render("favorite")
-    );
+    showFavoritesButton.addEventListener("click", () => {
+      todoList.render("favorite");
+      makeButtonStyleInactive();
+      showFavoritesButton.style.borderBottom = "5px solid blue";
+    });
   }
 
   if (showCompletedButton) {
-    showCompletedButton.addEventListener("click", () =>
-      todoList.render("completed")
-    );
+    showCompletedButton.addEventListener("click", () => {
+      todoList.render("completed");
+      makeButtonStyleInactive();
+      showCompletedButton.style.borderBottom = "5px solid blue";
+    });
   }
 
   if (showIncompleteButton) {
-    showIncompleteButton.addEventListener("click", () =>
-      todoList.render("incomplete")
-    );
+    showIncompleteButton.addEventListener("click", () => {
+      todoList.render("incomplete");
+      makeButtonStyleInactive();
+      showIncompleteButton.style.borderBottom = "5px solid blue";
+    });
   }
 
   showAll();
